2201f0b823c5ed279cfd3ecdaf7cc3ede8977dc1
17508777 <Antonov.F.V@sberbank.ru>
2019-10-28 13:36:34 +0300

    Изменение контроллеров логин и регистрации

--- src/main/java/hello/entities/Tobacco.java
+++ src/main/java/hello/entities/Tobacco.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Table(name = "tobacco", schema = "hookah")
 public class Tobacco {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     private String label;
     private int strength;
