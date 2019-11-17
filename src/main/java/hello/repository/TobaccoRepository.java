4406f4b4aa485fa16dc7d2294c5f2d2eb4c43608
Ivan <menkov_99@mail.ru>
2019-11-17 20:41:37 +0300

    findById

--- src/main/java/hello/repository/TobaccoRepository.java
+++ src/main/java/hello/repository/TobaccoRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface TobaccoRepository extends CrudRepository<Tobacco,Integer> {
     Tobacco findByLabel(String label);
     Tobacco findByFlavor(String flavor);
-    Tobacco findTobaccoById(Integer id);
 }
