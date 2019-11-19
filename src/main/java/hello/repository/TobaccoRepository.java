f07eb9bc1b0f542e7ead64c939abb7550a43a678
17508777 <Antonov.F.V@sberbank.ru>
2019-11-19 18:19:41 +0300

    flavor->flavour

--- src/main/java/hello/repository/TobaccoRepository.java
+++ src/main/java/hello/repository/TobaccoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TobaccoRepository extends CrudRepository<Tobacco,Integer> {
     Tobacco findByLabel(String label);
-    Tobacco findByFlavor(String flavor);
+    Tobacco findByFlavour(String flavour);
 }
